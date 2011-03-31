95efce13a1554958aa3782be308d731a27327504
msieber88@gmail.com <msieber88@gmail.com@4feab3de-de25-87cd-0e07-2f1f9e466e77>
2011-03-31 08:38:24 +0000

    GUI and GUI libs

---
+++ src/at/fhv/teamc/easydiet/view/EasyDietWindow.java
@@ -0,0 +1,32 @@
+/**
+ * This File is part of Easy Diet
+ * created on: 31.03.2011
+ * created by: Michael
+ * file: EasyDietWindow.java
+ */
+
+package at.fhv.teamc.easydiet.view;
+
+import java.net.URL;
+import org.apache.pivot.beans.Bindable;
+import org.apache.pivot.collections.Map;
+import org.apache.pivot.util.Resources;
+import org.apache.pivot.wtk.Window;
+
+/**
+ * Represents the application's main entry point into the UI
+ * @author Michael
+ */
+public class EasyDietWindow extends Window implements Bindable {
+
+    /**
+     * First called after creating the GUI
+     * @param map Contains all UI elements by id name
+     * @param url
+     * @param rsrcs
+     */
+    public void initialize(Map<String, Object> map, URL url, Resources rsrcs) {
+
+    }
+
+}
