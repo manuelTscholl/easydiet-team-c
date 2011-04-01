5ccd1372ad237b6b7f2bffd50d92251df91cedc8
msieber88@gmail.com <msieber88@gmail.com@4feab3de-de25-87cd-0e07-2f1f9e466e77>
2011-04-01 05:45:21 +0000

    GUI SuchTab fertiggestellt

--- src/at/fhv/teamc/easydiet/view/EasyDietWindow.java
+++ src/at/fhv/teamc/easydiet/view/EasyDietWindow.java
@@ -8,33 +8,29 @@
 package at.fhv.teamc.easydiet.view;
 
 import java.net.URL;
-import org.apache.pivot.beans.BXML;
 import org.apache.pivot.beans.Bindable;
 import org.apache.pivot.collections.Map;
 import org.apache.pivot.util.Resources;
-import org.apache.pivot.wtk.TablePane;
 import org.apache.pivot.wtk.Window;
 
 /**
- * Represents the application's main entry point into the UI
+ * Represents the application's main entry point into the UI (main.xml)
  * @author Michael
  */
 public class EasyDietWindow extends Window implements Bindable {
 
     // instance variables
-    @BXML private TablePane.Row _workSpaceRow;
 
 
     /**
      * First called after creating the GUI
-     * @param map Contains all UI elements by id name
+     * @param map Contains all UI elements in main.xml by id name
      * @param url Contains the URL of the main bxml file
      * @param rsrcs
      */
     public void initialize(Map<String, Object> map, URL url, Resources rsrcs) {
 
         // get GUI components
-        _workSpaceRow = (TablePane.Row)map.get("workSpaceRow");
     }
 
 }
---
+++ src/at/fhv/teamc/easydiet/view/NavigationTabPane.java
@@ -0,0 +1,42 @@
+/**
+ * This File is part of Easy Diet
+ * created on: 01.04.2011
+ * created by: Michael
+ * file: NavigationTabPane.java
+ */
+package at.fhv.teamc.easydiet.view;
+
+import java.net.URL;
+import org.apache.pivot.beans.BXML;
+import org.apache.pivot.beans.Bindable;
+import org.apache.pivot.collections.Map;
+import org.apache.pivot.util.Resources;
+import org.apache.pivot.wtk.BoxPane;
+import org.apache.pivot.wtk.TabPane;
+import org.apache.pivot.wtk.TextInput;
+
+/**
+ * Represents the application's navigation tabpane (navigation.bxml)
+ * @author Michael
+ */
+public class NavigationTabPane extends TabPane implements Bindable {
+
+    // instance variables
+    @BXML
+    private TextInput _searchTextInput;
+    @BXML
+    private BoxPane _searchResultBoxPane;
+
+    /**
+     * First called after creating the GUI
+     * @param map Contains all UI elements in navigation.bxml by id name
+     * @param url Contains the URL of the navigation bxml file
+     * @param rsrcs
+     */
+    public void initialize(Map<String, Object> map, URL url, Resources rsrcs) {
+
+        // get GUI components
+        _searchTextInput = (TextInput) map.get("searchTextInput");
+        _searchResultBoxPane = (BoxPane) map.get("searchResultBoxPane");
+    }
+}
