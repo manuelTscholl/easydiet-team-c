a0aaf78dda2c73dab98841829966e3b63601bedf
manuel.tscholl@gmail.com <manuel.tscholl@gmail.com@4feab3de-de25-87cd-0e07-2f1f9e466e77>
2011-04-05 07:54:44 +0000

    DatabaseIntarfaces
    Should be implemented foreach class/object which has do be saved to database.

---
+++ trunk/src/at/fhv/teamc/easydiet/model/Deleteable.java
@@ -0,0 +1,17 @@
+/**
+ * This file is part of Easy Diet.
+ *   created on: 05.04.2011
+ *   created by: Manuel
+ *   file: Deleteable.java
+ */
+package at.fhv.teamc.easydiet.model;
+
+/**
+ * @author Manuel
+ *
+ */
+public interface Deleteable
+{
+    boolean delete();
+
+}
