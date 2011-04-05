81cb8bb4f6f339480c1922a2a389c2f32481ee38
msieber88@gmail.com <msieber88@gmail.com@4feab3de-de25-87cd-0e07-2f1f9e466e77>
2011-04-05 16:52:21 +0000

    Weitergabe von Sucheingabe an GUIController, BusinessLogicController klasse erstellt, updateSearchResult implementiert

---
+++ trunk/src/at/fhv/teamc/easydiet/controller/BusinessLogicController.java
@@ -0,0 +1,25 @@
+/**
+ * This File is part of Easy Diet
+ * created on: 05.04.2011
+ * created by: Michael
+ * file: BusinesLogicController.java
+ */
+package at.fhv.teamc.easydiet.controller;
+
+/**
+ * Controller for business logic
+ * @author Michael
+ */
+public class BusinessLogicController {
+
+    // class variables
+    public static final org.apache.log4j.Logger LOGGER = org.apache.log4j.Logger.getLogger(BusinessLogicController.class);
+
+    /**
+     * Search patient in database
+     * @param search string
+     */
+    public void searchPatient(String search) {
+        LOGGER.trace("Sucheingabe: " + search);
+    }
+}
