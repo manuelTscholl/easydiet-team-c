09f111ab482ab0c7a128234a9cf1fd08aea03c77
msieber88@gmail.com <msieber88@gmail.com@4feab3de-de25-87cd-0e07-2f1f9e466e77>
2011-04-06 14:23:17 +0000

    icons

---
+++ src/at/fhv/teamc/easydiet/view/PatientDataListener.java
@@ -0,0 +1,19 @@
+/**
+ * This File is part of Easy Diet
+ * created on: 06.04.2011
+ * created by: Michael
+ * file: PatientDataListener.java
+ */
+
+package at.fhv.teamc.easydiet.view;
+
+import at.easydiet.model.Patient;
+
+/**
+ * Listener for patient data
+ * @author Michael
+ */
+public interface PatientDataListener {
+
+    public void updatePatientData(Patient p);
+}
