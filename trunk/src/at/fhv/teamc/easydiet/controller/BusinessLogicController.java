af19bd33ab037713cfdd18fd4ce89b7978b085ba
ali.guemues@gmail.com <ali.guemues@gmail.com@4feab3de-de25-87cd-0e07-2f1f9e466e77>
2011-04-11 12:33:40 +0000

    business

--- trunk/src/at/fhv/teamc/easydiet/controller/BusinessLogicController.java
+++ trunk/src/at/fhv/teamc/easydiet/controller/BusinessLogicController.java
@@ -7,6 +7,11 @@
 package at.fhv.teamc.easydiet.controller;
 
 import at.fhv.teamc.easydiet.model.PatientBo;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.util.Date;
+import javax.print.attribute.SetOfIntegerSyntax;
+import org.omg.CORBA.SystemException;
 
 /**
  * Controller for business logic
@@ -16,6 +21,12 @@ public class BusinessLogicController {
 
     // class variables
     public static final org.apache.log4j.Logger LOGGER = org.apache.log4j.Logger.getLogger(BusinessLogicController.class);
+    private String _name;
+    private int _svn;
+    private Date _bday;
+
+
+    DatabaseController controller = new DatabaseController();//einfach probe
 
     /**
      * Search patient in database
@@ -31,5 +42,61 @@ public class BusinessLogicController {
      */
     public void chooseActivePatient(PatientBo p) {
         LOGGER.trace("Patient: " + p.getInsuranceNumber());
+    }
+
+    /*
+     * This method calls the DB-controller which finally gets the required patient objects
+     */
+    public Set<PatientBo> getPatients(String search) {
+
+
+        String []t;
+
+        String name1 = "";
+        String name2 ="";
+        String SVN = "";
+        Date date = new Date();
+
+        String regexName = "/[a-z]{2,}/i";
+        String regexSVN = "/[0-9]{10}";
+        String regexDATE = "/[0-9]{1,2}[.][0-9]{1,2}[.][0-9]{1,4}";
+
+        t = search.split(" ");
+
+        if(t==null || t.length > 4) {
+
+            return null;
+        }
+
+        for(int i= 0; i<t.length; i++) {
+
+            if(t[i].matches(regexName)) {
+
+                if(name1.equals("")) {
+
+                    name1 = t[i];
+                }
+                else {
+                    name2 = t[i];
+                }
+            }
+            else if(t[i].matches(regexSVN)) {
+
+                SVN = t[i];
+            }
+            else if(t[i].matches(regexDATE)) {
+
+                try {
+                    date = DateFormat.getInstance().parse(t[i]);
+                }
+                catch(ParseException p) {
+
+                    
+                }
+            }
+
+           return controller.getPatients(name1, name2, SVN, date);
+
+        }
     }
 }
