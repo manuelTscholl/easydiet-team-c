2d1b36e335c7eb922199150c32b428537ded54be
manuel.tscholl@gmail.com <manuel.tscholl@gmail.com@4feab3de-de25-87cd-0e07-2f1f9e466e77>
2011-04-11 10:15:11 +0000

    Databasecontroller added method getPatient

---
+++ trunk/src/at/easydiet/dao/CheckOperatorDAO.java
@@ -0,0 +1,13 @@
+package at.easydiet.dao;
+
+import at.easydiet.model.CheckOperator;
+
+
+/**
+ * A DAO implementation for CheckOperator objects.
+ */
+public class CheckOperatorDAO 
+        extends GenericHibernateDAO<CheckOperator, Long>
+{
+	// implementation in parent class
+}
