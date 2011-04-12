044310c78bda06d2b2f6e4488900e2f55b5f9a2c
msieber88@gmail.com <msieber88@gmail.com@4feab3de-de25-87cd-0e07-2f1f9e466e77>
2011-04-12 07:14:10 +0000

    Sequenzdiagramm

--- src/at/fhv/teamc/easydiet/controller/BusinessLogicController.java
+++ src/at/fhv/teamc/easydiet/controller/BusinessLogicController.java
@@ -6,11 +6,8 @@
  */
 package at.fhv.teamc.easydiet.controller;
 
-import at.easydiet.model.Patient;
+import at.fhv.easydiet.controller.usecase.SearchPatientController;
 import at.fhv.teamc.easydiet.model.PatientBo;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.util.Date;
 import java.util.Set;
 
 /**
@@ -18,113 +15,39 @@ import java.util.Set;
  * 
  * @author Michael
  */
-public class BusinessLogicController
-{
+public class BusinessLogicController {
 
     // class variables
-    public static final org.apache.log4j.Logger LOGGER     = org.apache.log4j.Logger
-                                                                   .getLogger(BusinessLogicController.class);
-    private PatientBo _activePatient; 
+    public static final org.apache.log4j.Logger LOGGER = org.apache.log4j.Logger.getLogger(BusinessLogicController.class);
+    private PatientBo _activePatient;
+    SearchPatientController _searchPatientController;
 
-    // connects to the database and gets all the needed data.
-    DatabaseController                          controller = new DatabaseController();
+    public BusinessLogicController(){
+        _searchPatientController = new SearchPatientController();
+    }
 
     /**
      * Not implemented
      * 
      * @param search
      */
-    public void searchPatient(String search)
-    {
-        LOGGER.trace("Sucheingabe: " + search);
+    public Set<PatientData> searchPatient(String search) {
+            return _searchPatientController.getPatients(search);
     }
 
     /**
      * Sets the active Patient
      * @param p the patient which should be set to active
      */
-    public void setActivePatient(PatientBo p)
-    {
+    public void setActivePatient(PatientBo p) {
         _activePatient = p;
     }
-    
+
     /**
      * Gets the active Patient which was set from the GUI Controller before     * 
      * @param p
      */
-    public PatientBo getActivePatient()
-    {
+    public PatientBo getActivePatient() {
         return _activePatient;
-    }   
-    
-
-    /**
-     * This method calls the DB-controller which finally gets the required
-     * patient objects
-     * 
-     * @param search
-     *            the complete string which will be parsed in firstname,
-     *            secondname, svn and date
-     * @return a list of PatientBo's which matched to the specified search
-     *         string
-     */
-    public Set<PatientBo> getPatients(String search)
-    {
-
-        String[] t;
-
-        String name1 = "";
-        String name2 = "";
-        String SVN = "";
-        Date date = new Date();
-
-        String regexName = "/[a-z]{2,}/i";
-        String regexSVN = "/[0-9]{10}";
-        String regexDATE = "/[0-9]{1,2}[.][0-9]{1,2}[.][0-9]{1,4}";
-
-        t = search.split(" ");
-
-        if (t == null || t.length > 4)
-        {
-
-            return null;
-        }
-
-        for (int i = 0; i < t.length; i++)
-        {
-
-            if (t[i].matches(regexName))
-            {
-
-                if (name1.equals(""))
-                {
-
-                    name1 = t[i];
-                }
-                else
-                {
-                    name2 = t[i];
-                }
-            }
-            else if (t[i].matches(regexSVN))
-            {
-
-                SVN = t[i];
-            }
-            else if (t[i].matches(regexDATE))
-            {
-
-                try
-                {
-                    date = DateFormat.getInstance().parse(t[i]);
-                }
-                catch (ParseException p)
-                {
-
-                    LOGGER.error(p);
-                }
-            }
-        }
-        return controller.getPatients(name1, name2, SVN, date);
     }
 }
---
+++ src/at/fhv/teamc/easydiet/controller/PatientData.java
@@ -0,0 +1,13 @@
+/**
+ * This File is part of Easy Diet
+ * created on: 12.04.2011
+ * created by: Michael
+ * file: PatientData.java
+ */
+
+package at.fhv.teamc.easydiet.controller;
+
+
+public interface PatientData {
+
+}
