2c985db0f4cd82ea4f9043382c4a84564bf8f6a3
manuel.tscholl@gmail.com <manuel.tscholl@gmail.com@4feab3de-de25-87cd-0e07-2f1f9e466e77>
2011-04-14 18:58:26 +0000

    Structure complete?!

---
+++ src/at/easydiet/teamc/controller/PatientData.java
@@ -0,0 +1,19 @@
+/**
+ * This File is part of Easy Diet
+ * created on: 12.04.2011
+ * created by: Michael
+ * file: PatientData.java
+ */
+
+package at.easydiet.teamc.controller;
+
+import java.util.Date;
+
+
+public interface PatientData {
+
+    public String getForename();
+    public String getLastname();
+    public Date getBirthday();
+    public String getInsuranceNumber();
+}
---
+++ src/at/easydiet/teamc/view/GUIComponents.java
@@ -0,0 +1,38 @@
+/**
+ * This File is part of Easy Diet
+ * created on: 11.04.2011
+ * created by: Michael
+ * file: GUIComponents.java
+ */
+package at.easydiet.teamc.view;
+
+import java.util.HashMap;
+
+/**
+ * Contains all registered GUI Components by id
+ * @author Michael
+ */
+public class GUIComponents {
+
+    // class variables
+    public static final org.apache.log4j.Logger LOGGER = org.apache.log4j.Logger.getLogger(ContentTabPane.class);
+    private static HashMap<String, Object> sCOMPONENTS = new HashMap<String, Object>() {};
+
+    /**
+     * Get a component by his id
+     * @param id
+     * @return
+     */
+    public static Object get(String id) {
+        return sCOMPONENTS.get(id);
+    }
+
+    /**
+     * Add a new component
+     * @param id Component name
+     * @param o Component to add
+     */
+    public static void put(String id, Object o) {
+        sCOMPONENTS.put(id, o);
+    }
+}
