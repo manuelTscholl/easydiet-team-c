e110ee4dbfaf3eb1392233d32321128cdf134c22
fboesch90@gmail.com <fboesch90@gmail.com@4feab3de-de25-87cd-0e07-2f1f9e466e77>
2011-04-18 13:52:46 +0000

    DietParameterTemplateData hinzugefügt

---
+++ trunk/src/at/easydiet/teamc/controller/usecase/SearchParameterController.java
@@ -0,0 +1,41 @@
+package at.easydiet.teamc.controller.usecase;
+
+import java.util.Set;
+
+import at.easydiet.teamc.controller.BusinessLogicDelegationController;
+import at.easydiet.teamc.controller.DatabaseController;
+import at.easydiet.teamc.model.DietParameterTemplateBo;
+import at.easydiet.teamc.model.data.PatientData;
+
+public class SearchParameterController {
+	
+	// class variables
+    public static final org.apache.log4j.Logger LOGGER = org.apache.log4j.Logger.getLogger(SearchParameterController.class);
+    private static volatile SearchParameterController _searchParameterController = null;
+    //instance variables
+    private DatabaseController _dbController;
+    private Set<PatientData> _lastSearchResult;
+
+    
+	/**
+     * Singelton
+     * @return Will return the existing Instance or if no exists a new Instance of {@link SearchParameterController}
+     */
+	public static SearchParameterController getInstance() {
+		if(_searchParameterController==null){
+			_searchParameterController=new SearchParameterController();
+		}
+		return _searchParameterController;
+	}
+
+	/**
+	 * Returns a set of all DietParameterTemplateBo's
+	 * @return
+	 */
+	public Set<DietParameterTemplateBo> getParameters() {
+		Set<DietParameterTemplateBo> parameters=_dbController.getParameters();
+		return parameters;
+	}
+	
+	
+}
---
+++ trunk/src/at/easydiet/teamc/model/data/DietParameterTemplateData.java
@@ -0,0 +1,19 @@
+/**
+ * This File is part of Easy Diet
+ * created on: 12.04.2011
+ * created by: Friedrich Bösch
+ * file: DietParameterTemplateData.java
+ */
+package at.easydiet.teamc.model.data;
+
+public interface DietParameterTemplateData {
+	
+	//parameter name,wert und comparerer
+	
+	String getParameterName();
+	String getParameterValue();
+	
+	
+	
+
+}
