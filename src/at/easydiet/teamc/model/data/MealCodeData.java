47c25946784647a798019592d2c22bec1c276199
ali.guemues@gmail.com <ali.guemues@gmail.com@4feab3de-de25-87cd-0e07-2f1f9e466e77>
2011-04-22 08:31:36 +0000

    MealCodeData, MealData

---
+++ src/at/easydiet/teamc/model/data/MealCodeData.java
@@ -0,0 +1,14 @@
+/**
+ * This File is part of Easy Diet
+ * created on: 18.04.2011
+ * created by: Ali Gümüs
+ * file: MealCodeData.java
+ */
+
+package at.easydiet.teamc.model.data;
+
+public interface MealCodeData {
+
+    String getMealName();
+    String getMealCode();
+}
