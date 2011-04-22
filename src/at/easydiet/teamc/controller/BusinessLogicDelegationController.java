a24be1854055af2cfab5252b271a97cec06aae85
fboesch90@gmail.com <fboesch90@gmail.com@4feab3de-de25-87cd-0e07-2f1f9e466e77>
2011-04-22 08:38:38 +0000

    BusinessLogicDelegationController methoden implementiert

--- src/at/easydiet/teamc/controller/BusinessLogicDelegationController.java
+++ src/at/easydiet/teamc/controller/BusinessLogicDelegationController.java
@@ -1,7 +1,7 @@
 /**
  * This File is part of Easy Diet
  * created on: 05.04.2011
- * created by: Michael
+ * created by: Friedrich Bösch
  * file: BusinesLogicController.java
  */
 package at.easydiet.teamc.controller;
@@ -21,6 +21,7 @@ import at.easydiet.teamc.util.EventArgs;
 import at.easydiet.teamc.util.IEventHandler;
 import at.easydiet.teamc.model.*;
 import at.easydiet.teamc.model.data.CheckedRecipeVo;
+import at.easydiet.teamc.model.data.DietParameterData;
 import at.easydiet.teamc.model.data.DietryPlanData;
 import at.easydiet.teamc.model.data.MealCodeData;
 import at.easydiet.teamc.model.data.MealData;
@@ -29,196 +30,174 @@ import at.easydiet.teamc.model.data.RecipeData;
 /**
  * Controller for business logic
  * 
- * @author Michael
  */
-public class BusinessLogicDelegationController
-{
+public class BusinessLogicDelegationController {
 
-    // class variables
-    public static final org.apache.log4j.Logger               LOGGER = org.apache.log4j.Logger
-                                                                             .getLogger(BusinessLogicDelegationController.class);
-    private static volatile BusinessLogicDelegationController _businessLogicDelegationController;
-    // instance variables
-    private PatientBo                                         _activePatient;
-    private SearchPatientController                           _searchPatientController;
-    private SearchParameterController                         _searchParameterController;
-    private Thread                                            _patientSearchThread;
+	// class variables
+	public static final org.apache.log4j.Logger LOGGER = org.apache.log4j.Logger
+			.getLogger(BusinessLogicDelegationController.class);
+	private static volatile BusinessLogicDelegationController _businessLogicDelegationController;
+	// instance variables
+	private PatientBo _activePatient;
+	private SearchPatientController _searchPatientController;
+	private SearchParameterController _searchParameterController;
+	private Thread _patientSearchThread;
 
-    private DietryPlanController _dietryPlanController;
-    /**
-     * Initializes a new instance of the
-     * {@link BusinessLogicDelegationController} class.
-     * 
-     * @param sender
-     */
-    private BusinessLogicDelegationController()
-    {
-        _searchPatientController = SearchPatientController.getInstance();
-        _searchParameterController = SearchParameterController.getInstance();
-        _searchPatientController = SearchPatientController.getInstance();
-    }
+	private DietryPlanController _dietryPlanController;
 
-    /**
-     * Gets the lastSearchResult.
-     * 
-     * @return the lastSearchResult
-     */
-    public synchronized Set<PatientData> getLastPatientSearchResult()
-    {
-        return _searchPatientController.getLastSearchResult();
-    }
+	/**
+	 * Initializes a new instance of the
+	 * {@link BusinessLogicDelegationController} class.
+	 * 
+	 * @param sender
+	 */
+	private BusinessLogicDelegationController() {
+		_searchPatientController = SearchPatientController.getInstance();
+		_searchParameterController = SearchParameterController.getInstance();
+		_searchPatientController = SearchPatientController.getInstance();
+	}
 
-    /**
-     * Singelton: creates or returns the last created instance
-     * 
-     * @return
-     */
-    public static BusinessLogicDelegationController getInstance()
-    {
-        if (_businessLogicDelegationController == null)
-        {
-            _businessLogicDelegationController = new BusinessLogicDelegationController();
-        }
+	/**
+	 * Gets the lastSearchResult.
+	 * 
+	 * @return the lastSearchResult
+	 */
+	public synchronized Set<PatientData> getLastPatientSearchResult() {
+		return _searchPatientController.getLastSearchResult();
+	}
 
-        return _businessLogicDelegationController;
-    }
+	/**
+	 * Singelton: creates or returns the last created instance
+	 * 
+	 * @return
+	 */
+	public static BusinessLogicDelegationController getInstance() {
+		if (_businessLogicDelegationController == null) {
+			_businessLogicDelegationController = new BusinessLogicDelegationController();
+		}
 
-    /**
-     * Searches patients in database, will start an asyncron searching Thread
-     * @param search
-     * @return The last list of Patients which where found in database, may not the patients from the actual search for actual search use the events
-     */
-    public Set<PatientData> searchPatient(String search)
-    {
+		return _businessLogicDelegationController;
+	}
 
-        // start search only if at least 2 characters are available
-        if (search.length() >= 2)
-        {
-            _searchPatientController.addNewPatientSearchString(search);
+	/**
+	 * Searches patients in database, will start an asyncron searching Thread
+	 * 
+	 * @param search
+	 * @return The last list of Patients which where found in database, may not
+	 *         the patients from the actual search for actual search use the
+	 *         events
+	 */
+	public Set<PatientData> searchPatient(String search) {
 
-            //starting the search loop if not already started
-            if (_patientSearchThread == null)
-            {
-                _patientSearchThread = new Thread(_searchPatientController);
-                _patientSearchThread.setName("PatientSearchThread");
-                _patientSearchThread.start();
-            }
-            
-            // let the gui layer know that something updated
+		// start search only if at least 2 characters are available
+		if (search.length() >= 2) {
+			_searchPatientController.addNewPatientSearchString(search);
 
-            return getLastPatientSearchResult();
-        }
+			// starting the search loop if not already started
+			if (_patientSearchThread == null) {
+				_patientSearchThread = new Thread(_searchPatientController);
+				_patientSearchThread.setName("PatientSearchThread");
+				_patientSearchThread.start();
+			}
 
-        return null;
-    }
+			// let the gui layer know that something updated
 
-    /**
-     * Sets the active Patient
-     * 
-     * @param p
-     *            the patient which should be set to active
-     */
-    public void setActivePatient(PatientData p)
-    {
-        _activePatient = (PatientBo) p;
-    }
+			return getLastPatientSearchResult();
+		}
 
-    /**
-     * Gets the active Patient which was set from the GUI Controller before *
-     * 
-     * @param p
-     */
-    public PatientBo getActivePatient()
-    {
-        return _activePatient;
-    }
+		return null;
+	}
 
-    public void addNewPatientSearchHandler(IEventHandler<EventArgs> handler)
-    {
-        _searchPatientController.addHandler(handler);
-    }
+	/**
+	 * Sets the active Patient
+	 * 
+	 * @param p
+	 *            the patient which should be set to active
+	 */
+	public void setActivePatient(PatientData p) {
+		_activePatient = (PatientBo) p;
+	}
 
-    
-    public Set<DietParameterTemplateData> getParameters(){
-    	  	
-    	/*Set<PatientData> patientDatas = new HashSet<PatientData>();
-        Set<PatientBo> patients = _dbController.getPatients(name1, name2, svn, date);
+	/**
+	 * Gets the active Patient which was set from the GUI Controller before *
+	 * 
+	 * @param p
+	 */
+	public PatientBo getActivePatient() {
+		return _activePatient;
+	}
 
-        // check if database set is not empty
-        if (patients != null) {
+	public void addNewPatientSearchHandler(IEventHandler<EventArgs> handler) {
+		_searchPatientController.addHandler(handler);
+	}
 
-            // convert business patient data to gui patient data
-            for (PatientBo pbo : patients) {
-                patientDatas.add(pbo);
-            }
-        }*/
-    	
-    	Set<DietParameterTemplateData> parameterData=new HashSet<DietParameterTemplateData>();    	
-    	Set<DietParameterTemplateBo> parameters=_searchParameterController.getParameters();  
-    	
-    	if(parameters!=null){
-    		//convert business dietparameter to dietparameterdata
-//    		for(DietParameterTemplateBo dbo:parameters){
-//    			parameterData.add(dbo);
-//    		}
-    	}
-    	    	
-    	
-    	return null;
-    }
+	public Set<DietParameterData> getParameters() {
 
-    /*
+		Set<DietParameterBo> parameterBo = _searchParameterController
+				.getParameters();
+		// change DietParameterBo's in paramterBo to DietParameterData
+
+		Set<DietParameterData> parameterData = new HashSet<DietParameterData>();
+
+		for (DietParameterBo dpBo : parameterBo) {
+			parameterData.add(dpBo);
+		}
+		return null;
+	}
+
+	/*
      * 
      */
-     public Set<MealCodeData> getAllMeals() {
+	public Set<MealCodeData> getAllMeals() {
 
-         _dietryPlanController.getAllMeals();
-         Set<MealBo> mealBo = new HashSet<MealBo>();
-         MealCodeData mealData = new MealCodeData() {
+		_dietryPlanController.getAllMeals();
+		Set<MealBo> mealBo = new HashSet<MealBo>();
+		MealCodeData mealData = new MealCodeData() {
 
-            public String getMealName() {
-                throw new UnsupportedOperationException("Not supported yet.");
-            }
+			public String getMealName() {
+				throw new UnsupportedOperationException("Not supported yet.");
+			}
 
-            public String getMealCode() {
-                throw new UnsupportedOperationException("Not supported yet.");
-            }
-        };
+			public String getMealCode() {
+				throw new UnsupportedOperationException("Not supported yet.");
+			}
+		};
 
-        return null;
-    }
+		return null;
+	}
 
-    public void addMealCodeData(MealCodeData mcd) {
+	public void addMealCodeData(MealCodeData mcd) {
 
-    }
+	}
 
-    public int addMealLine() {
+	public int addMealLine() {
 
-        throw new UnsupportedOperationException("Not supported yet!");
-    }
+		throw new UnsupportedOperationException("Not supported yet!");
+	}
 
-    public Set<RecipeData> getRecipeMainCategories() {
+	public Set<RecipeData> getRecipeMainCategories() {
 
-        throw new UnsupportedOperationException("Not supported yet!");
-    }
+		throw new UnsupportedOperationException("Not supported yet!");
+	}
 
-    public Set<CheckedRecipeVo> searchRecipe(String mainCategorie, String search) {
+	public Set<CheckedRecipeVo> searchRecipe(String mainCategorie, String search) {
 
-        throw new UnsupportedOperationException("Not supported yet!");
-    }
-    
-    public MealData addMealtoRecipe(RecipeData rd, double quantity, int mealLineTD) {
+		throw new UnsupportedOperationException("Not supported yet!");
+	}
 
-        throw new UnsupportedOperationException("Not supported yet!");
-    }
+	public MealData addMealtoRecipe(RecipeData rd, double quantity,
+			int mealLineTD) {
 
-    public void saveDietry() {
+		throw new UnsupportedOperationException("Not supported yet!");
+	}
 
-        throw new UnsupportedOperationException("Not supported yet!");
-    }
+	public void saveDietry() {
 
-    public DietryPlanData getDietryPlan() {
+		throw new UnsupportedOperationException("Not supported yet!");
+	}
 
-        throw new UnsupportedOperationException("Not supported yet!");
-    }
+	public DietryPlanData getDietryPlan() {
+
+		throw new UnsupportedOperationException("Not supported yet!");
+	}
 }
