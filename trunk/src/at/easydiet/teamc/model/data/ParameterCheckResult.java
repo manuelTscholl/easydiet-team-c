7077b6080af2dfb7effc5ac4d4ef2ace158d24e6
xandor9@hotmail.com <xandor9@hotmail.com@4feab3de-de25-87cd-0e07-2f1f9e466e77>
2011-04-23 11:25:55 +0000

    DietryPlanController enthält keine leerimplementierungen mehr, todos noch nicht bearbeitet DietPlanBo.addMealCode gibt nun MealBo zurück → aktuell bearbeitetes Meal kann sonst nicht gecached werden wovon aber im sequenzdiagramm ausgegangen wird
    final int MILLISECONDS_TO_DAY_FACTOR eingeführt da häufig gebrauchte konstante in dietaryplancontroller
    
    PlanTypeBo wird vorläufig immer mit „Testplan“ gefüllt
    
    ParameterCheckResult.java als leerimplementierung erstellt

---
+++ trunk/src/at/easydiet/teamc/model/data/ParameterCheckResult.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package at.easydiet.teamc.model.data;
+
+/**
+ *
+ * @author Stephan
+ */
+public class ParameterCheckResult {
+
+    //TODO whole implementation of this class an determination wether class will be interface instead.
+
+}
