f161d58423b14fcac5d8ccb077c29b1282cea1ad
xandor9@hotmail.com <xandor9@hotmail.com@4feab3de-de25-87cd-0e07-2f1f9e466e77>
2011-04-26 12:39:40 +0000

    CheckedRecipeVo nun auch mit ParameterCheckResult

--- src/at/easydiet/teamc/model/data/CheckedRecipeVo.java
+++ src/at/easydiet/teamc/model/data/CheckedRecipeVo.java
@@ -14,11 +14,15 @@ public class CheckedRecipeVo {
     // instance variables
     private RecipeData _recipeData;
     private ParameterCheckResult _checkResult;
+    private boolean _disfavour;
 
     public CheckedRecipeVo(RecipeData recipeData, ParameterCheckResult checkRecipeWithParameters) {
+        _disfavour=false;
         _recipeData = recipeData;
         _checkResult = checkRecipeWithParameters;
     }
+
+
 
     public ParameterCheckResult getCheckResult() {
         return _checkResult;
@@ -26,6 +30,20 @@ public class CheckedRecipeVo {
 
     public RecipeData getRecipeData() {
         return _recipeData;
+    }
+
+    /**
+     * @return the _disfavour
+     */
+    public boolean isDisfavour() {
+        return _disfavour;
+    }
+
+    /**
+     * @param disfavour the _disfavour to set
+     */
+    public void setDisfavour(boolean disfavour) {
+        this._disfavour = disfavour;
     }
 
 }
