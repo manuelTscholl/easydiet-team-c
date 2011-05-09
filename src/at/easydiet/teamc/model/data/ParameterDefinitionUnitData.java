039c6ae021f05882a9040909158ec78d14e09704
manuel.tscholl@gmail.com <manuel.tscholl@gmail.com@4feab3de-de25-87cd-0e07-2f1f9e466e77>
2011-05-09 07:48:48 +0000

    getName

---
+++ src/at/easydiet/teamc/model/data/ParameterDefinitionUnitData.java
@@ -0,0 +1,17 @@
+/**
+ * This file is part of Easy Diet.
+ *   created on: 09.05.2011
+ *   created by: Manuel
+ *   file: ParameterDefinitionUnitData.java
+ */
+package at.easydiet.teamc.model.data;
+
+/**
+ * @author Manuel
+ *
+ */
+public interface ParameterDefinitionUnitData
+{
+
+    public String getName();
+}
