8f12417340e88f66a31d9f9deab7fa35da8a0883
manuel.tscholl@gmail.com <manuel.tscholl@gmail.com@4feab3de-de25-87cd-0e07-2f1f9e466e77>
2011-05-09 07:43:58 +0000

    getallparameterdefinition getallparameterunits

--- trunk/src/at/easydiet/teamc/controller/DatabaseController.java
+++ trunk/src/at/easydiet/teamc/controller/DatabaseController.java
@@ -17,6 +17,8 @@ import at.easydiet.dao.DietParameterDAO;
 import at.easydiet.dao.PatientDAO;
 import at.easydiet.dao.RecipeDAO;
 import at.easydiet.model.DietParameter;
+import at.easydiet.model.ParameterDefinition;
+import at.easydiet.model.ParameterDefinitionUnit;
 import at.easydiet.model.Patient;
 import at.easydiet.model.Recipe;
 import at.easydiet.teamc.dao.MealCodeDao;
@@ -25,111 +27,125 @@ import at.easydiet.teamc.model.MealCodeBo;
 import at.easydiet.teamc.model.ParameterDefinitionBo;
 import at.easydiet.teamc.model.PatientBo;
 import at.easydiet.teamc.model.RecipeBo;
+import at.easydiet.teamc.model.ParameterDefinitionUnitBo;
+import at.easydiet.teamc.model.data.ParameterDefinitionUnitData;
 
-/** 
+/**
  * Will get all data from database via the different class dow's
+ * 
  * @author Manuel
- *
+ * 
  */
-public class DatabaseController {
+public class DatabaseController
+{
 
-    public static final org.apache.log4j.Logger LOGGER = org.apache.log4j.Logger.getLogger(DatabaseController.class);
-    private static DatabaseController _databaseController;
-
-    /** 
-     * Initializes a new instance of the {@link DatabaseController} class. 
-     */
-    private DatabaseController() {
-    }
+    public static final org.apache.log4j.Logger LOGGER = org.apache.log4j.Logger
+                                                               .getLogger(DatabaseController.class);
+    private static DatabaseController           _databaseController;
 
     /**
-     * Searches in the Database for the special Patient 
+     * Initializes a new instance of the {@link DatabaseController} class.
+     */
+    private DatabaseController()
+    {}
+
+    /**
+     * Searches in the Database for the special Patient
+     * 
      * @param name1
      * @param name
-     * @param svn the insurance Number of the Patient
+     * @param svn
+     *            the insurance Number of the Patient
      * @param birthday
      * @return all Patients with the wanted data as a HashSet
      */
-    public Set<PatientBo> getPatients(String name1, String name2, String svn, Date birthday) {
+    public Set<PatientBo> getPatients(String name1, String name2, String svn,
+            Date birthday)
+    {
 
         PatientDAO patientDao = DAOFactory.getInstance().getPatientDAO();
         List<Patient> patients = new ArrayList<Patient>();
         Set<PatientBo> patientsBo = new HashSet<PatientBo>();
 
-        patients = patientDao.findBySpecialProperties(name1, name2, svn, birthday);
+        patients = patientDao.findBySpecialProperties(name1, name2, svn,
+                birthday);
 
         // create Bo list
-        for(Patient p:patients){
+        for (Patient p : patients)
+        {
             patientsBo.add(new PatientBo(p));
         }
 
         return patientsBo;
     }
 
-
     /**
      * Singelton creates a new instance of this class or returns the existing
+     * 
      * @return
      */
     public static DatabaseController getInstance()
     {
-        if(_databaseController==null)
+        if (_databaseController == null)
             _databaseController = new DatabaseController();
-        
+
         return _databaseController;
     }
 
     /**
      * Finds all parameters in the database
+     * 
      * @return
      */
-	public List<DietParameterBo> getAllParameters() {
-		
-	    DietParameterDAO dietParameterDao = DAOFactory.getInstance().getDietParameterDAO();
-	    List<DietParameterBo> parametersBo = new ArrayList<DietParameterBo>();
-	    List<DietParameter> parameters = dietParameterDao.findAll();
-	    
-	    
-	    //creating Bos
-	    if(parameters!=null)
-	    for (DietParameter item : parameters)
+    public List<DietParameterBo> getAllParameters()
+    {
+
+        DietParameterDAO dietParameterDao = DAOFactory.getInstance()
+                .getDietParameterDAO();
+        List<DietParameterBo> parametersBo = new ArrayList<DietParameterBo>();
+        List<DietParameter> parameters = dietParameterDao.findAll();
+
+        // creating Bos
+        if (parameters != null) for (DietParameter item : parameters)
         {
             parametersBo.add(new DietParameterBo(item));
         }
 
-		return parametersBo;
-	}
-	
-	/**
-	 * Looks in database for all saved mealCodes, uses the MealDao for loading
-	 * @return a Set of meals loaded from database via hibernate
-	 */
+        return parametersBo;
+    }
+
+    /**
+     * Looks in database for all saved mealCodes, uses the MealDao for loading
+     * 
+     * @return a Set of meals loaded from database via hibernate
+     */
     public List<MealCodeBo> getAllMealCodes()
     {
         MealCodeDao mealCodeDao = new MealCodeDao();
-        
+
         List<MealCodeBo> mealCodes = mealCodeDao.findAll();
-        
+
         return mealCodes;
     }
-    
+
     /**
      * Gets all Bls categories out of the db
+     * 
      * @return a list of categories
      */
     public List<RecipeBo> getRecipeMainCategories()
-    {        
-        List<Recipe> recipes = DAOFactory.getInstance().getRecipeDAO().getCategories();
-        List<RecipeBo> recipesBo= new ArrayList<RecipeBo>();
-        
-        if(recipes!=null)
-            for (Recipe item : recipes)
-            {
-                recipesBo.add(new RecipeBo(item));
-            }
+    {
+        List<Recipe> recipes = DAOFactory.getInstance().getRecipeDAO()
+                .getCategories();
+        List<RecipeBo> recipesBo = new ArrayList<RecipeBo>();
+
+        if (recipes != null) for (Recipe item : recipes)
+        {
+            recipesBo.add(new RecipeBo(item));
+        }
         return recipesBo;
     }
-   
+
     /**
      * {@link RecipeDAO#searchRecipe(String blsCategorie,String name)}
      */
@@ -138,20 +154,47 @@ public class DatabaseController {
         RecipeDAO recipeDao = DAOFactory.getInstance().getRecipeDAO();
         List<RecipeBo> recipesBo = new ArrayList<RecipeBo>();
         List<Recipe> recipes = recipeDao.searchRecipe(blsCategorie, name);
-        
-        //creating Bos
-        if(recipes!=null)
-        for (Recipe recipe : recipes)
+
+        // creating Bos
+        if (recipes != null) for (Recipe recipe : recipes)
         {
             recipesBo.add(new RecipeBo(recipe));
         }
         return recipesBo;
     }
 
-	public List<ParameterDefinitionBo> getAllNutrimentParameters() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    /**
+     * Gets all Parameter definitions from the database (Alanyn, kj....)
+     * @return 
+     */
+    public List<ParameterDefinitionBo> getAllParameterDefinitions()
+    {
+        List<ParameterDefinition> datas = DAOFactory.getInstance().getParameterDefinitionDAO().findAll();
+        List<ParameterDefinitionBo> dataBos = new ArrayList<ParameterDefinitionBo>();
+        if(datas!=null)
+            for (ParameterDefinition parameterDefinition : datas)
+            {
+                dataBos.add(new ParameterDefinitionBo(parameterDefinition));
+            }
+        
+        return dataBos;
+    }
     
-    
+    /**
+     * 
+     * @return
+     */
+    public List<ParameterDefinitionUnitBo> getAllParameterDefinitionUnits()
+    {
+        List<ParameterDefinitionUnit> datas = DAOFactory.getInstance().getParameterDefinitionUnitDAO().findAll();
+        List<ParameterDefinitionUnitBo> dataBos = new ArrayList<ParameterDefinitionUnitBo>();
+        if(datas!=null)
+            for (ParameterDefinitionUnit parameterDefinition : datas)
+            {
+                dataBos.add(new ParameterDefinitionUnitBo(parameterDefinition));
+            }
+        
+        return dataBos;
+    }
+
 }
