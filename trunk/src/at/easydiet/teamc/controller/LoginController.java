1d978e97eea9483b735e824816cc7fe5095204b4
msieber88@gmail.com <msieber88@gmail.com@4feab3de-de25-87cd-0e07-2f1f9e466e77>
2011-05-10 13:58:59 +0000

    GUI bugfixes

--- trunk/src/at/easydiet/teamc/controller/LoginController.java
+++ trunk/src/at/easydiet/teamc/controller/LoginController.java
@@ -6,7 +6,6 @@
  */
 package at.easydiet.teamc.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import at.easydiet.dao.DAOFactory;
@@ -20,53 +19,48 @@ import at.easydiet.teamc.model.SystemUserBo;
  * @author Manuel
  * 
  */
-public class LoginController
-{
-    public static final org.apache.log4j.Logger LOGGER = org.apache.log4j.Logger
-                                                               .getLogger(LoginController.class);
+public class LoginController {
+	public static final org.apache.log4j.Logger LOGGER = org.apache.log4j.Logger
+			.getLogger(LoginController.class);
 
-    private static LoginController              _loginController;
-    private SystemUserBo                          _actualUser;
+	private static LoginController _loginController;
+	private SystemUserBo _actualUser;
 
-    /**
-     * Singelton: Constructor
-     * 
-     * @return
-     */
-    public static LoginController getInstance()
-    {
-        if (_loginController == null)
-        {
-            _loginController = new LoginController();
-        }
+	/**
+	 * Singelton: Constructor
+	 * 
+	 * @return
+	 */
+	public static LoginController getInstance() {
+		if (_loginController == null) {
+			_loginController = new LoginController();
+		}
 
-        return _loginController;
-    }
-    
-    /**
-     * Gets the actualUser or null if no one exists
-     * @return the actualUser
-     */
-    public SystemUserBo getActualUser()
-    {
-        //FIXME only for testing there is no user logined in the system
-        SystemUserDAO getUserDao = DAOFactory.getInstance().getSystemUserDAO();
-        List<SystemUser> users = getUserDao.findAll();
-        if(users.size()>=1)
-        {
-            return new SystemUserBo(users.get(0));
-        }
-        return null;
-        //return _actualUser;
-    }
+		return _loginController;
+	}
 
-    /**
-     * Sets the actualUser.
-     * @param actualUser the actualUser to set
-     */
-    public void setActualUser(SystemUserBo actualUser)
-    {
-        _actualUser = actualUser;
-    }
+	/**
+	 * Gets the actualUser or null if no one exists
+	 * 
+	 * @return the actualUser
+	 */
+	public SystemUserBo getActualUser() {
+		// FIXME only for testing there is no user logined in the system
+		SystemUserDAO getUserDao = DAOFactory.getInstance().getSystemUserDAO();
+		List<SystemUser> users = getUserDao.findAll();
+		if (users.size() >= 1) {
+			return new SystemUserBo(users.get(0));
+		}
+		return null;
+	}
+
+	/**
+	 * Sets the actualUser.
+	 * 
+	 * @param actualUser the actualUser to set
+	 */
+	public void setActualUser(SystemUserBo actualUser) {
+		_actualUser = actualUser;
+	}
 
 }
