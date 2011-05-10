3bd981d942b1631806846dd64f7893e1c4b02b85
fboesch90@gmail.com <fboesch90@gmail.com@4feab3de-de25-87cd-0e07-2f1f9e466e77>
2011-05-10 07:14:01 +0000

    minor changes with addParameter

--- trunk/src/at/easydiet/teamc/model/NutrimentRulesBo.java
+++ trunk/src/at/easydiet/teamc/model/NutrimentRulesBo.java
@@ -27,8 +27,9 @@ public class NutrimentRulesBo {
 		if (!_parameters.containsKey(parameterdefintion.getName())) {
 			_parameters.put(parameterdefintion.getName(), new NutrimentRuleBo(
 					parameterdefintion, checkOperatorBo, value));
-		}else{
-			//TODO what to do if already added? call changeParameter or throw error message?
+		} else {
+			// TODO what to do if already added? call changeParameter or throw
+			// error message?
 		}
 	}
 
@@ -40,6 +41,13 @@ public class NutrimentRulesBo {
 		List<NutrimentRuleBo> currParams = new ArrayList<NutrimentRuleBo>();
 		Set<NutrimentParameterBo> paramsOfRecipe = recipe
 				.getNutrimentParameters();
+		//if there are no parameters availible of the recipe, add them without comparing
+		if (recipe.getNutrimentParameters().size() == 0) {
+			for (NutrimentRuleBo rb : this._parameters.values()) {
+				currParams.add(rb);
+			}
+			return currParams;
+		}
 
 		for (NutrimentParameterBo npbo : recipe.getNutrimentParameters()) {
 			NutrimentRuleBo currentPar = _parameters.get(npbo
@@ -53,18 +61,36 @@ public class NutrimentRulesBo {
 					} else {
 						currentPar.setIsViolated(false);
 					}
-				} else if (currentPar.getCheckOperator().equals(">=")) {
-					if (currValue < Double.parseDouble(npbo.getValue())) {
+				} else if (currentPar.getCheckOperator().equals("!=")) {
+					if (currValue == Double.parseDouble(npbo.getValue())) {
 						currentPar.setIsViolated(true);
 					} else {
 						currentPar.setIsViolated(false);
 					}
-				}// TODO implement missing checkoperators
-				currParams.add(currentPar);
-			}
+				} else if (currentPar.getCheckOperator().equals("=")) {
+					if (currValue != Double.parseDouble(npbo.getValue())) {
+						currentPar.setIsViolated(true);
+					} else {
+						currentPar.setIsViolated(false);
+					}
+				} else if (currentPar.getCheckOperator().equals(">")) {
+					if (currValue <= Double.parseDouble(npbo.getValue())) {
+						currentPar.setIsViolated(true);
+					} else {
+						currentPar.setIsViolated(false);
+					}
+				} else if (currentPar.getCheckOperator().equals("<")) {
+					if (currValue >= Double.parseDouble(npbo.getValue())) {
+						currentPar.setIsViolated(true);
+					} else {
+						currentPar.setIsViolated(false);
+					}
+				}
 
+			}// TODO implement missing checkoperators
+			currParams.add(currentPar);
 		}
+
 		return currParams;
 	}
-
 }
