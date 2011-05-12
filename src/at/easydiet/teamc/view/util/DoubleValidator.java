fae3163baf7710c9b1f44678fddb33ee0b685ce3
msieber88@gmail.com <msieber88@gmail.com@4feab3de-de25-87cd-0e07-2f1f9e466e77>
2011-05-12 07:14:22 +0000

    double validator fix

--- src/at/easydiet/teamc/view/util/DoubleValidator.java
+++ src/at/easydiet/teamc/view/util/DoubleValidator.java
@@ -19,7 +19,7 @@ import org.apache.pivot.wtk.validation.Validator;
  */
 public class DoubleValidator implements Validator {
 
-	protected static final DecimalFormat FORMAT = new DecimalFormat("0.00");
+	protected static final DecimalFormat FORMAT = new DecimalFormat("0.0");
 	static {
 		FORMAT.setParseBigDecimal(true);
 	}
@@ -28,16 +28,20 @@ public class DoubleValidator implements Validator {
 	public boolean isValid(String text) {
 		boolean valid = false;
 
+		if (text.contains(",")) {
+			return false;
+		}
+
 		// check for valid input
 		if (text.length() > 0) {
 
 			ParsePosition parsePosition = new ParsePosition(0);
 			BigDecimal numericAmount = (BigDecimal) FORMAT.parse(text,
 					parsePosition);
+
 			valid = (numericAmount != null && numericAmount.scale() <= 2
-					&& numericAmount.signum() >= 0
-					&& parsePosition.getErrorIndex() == -1 && parsePosition
-					.getIndex() == text.length());
+					&& numericAmount.signum() >= 0 && parsePosition
+					.getErrorIndex() == -1);
 		}
 
 		return valid;
