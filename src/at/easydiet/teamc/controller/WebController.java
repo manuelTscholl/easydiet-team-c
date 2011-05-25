a61f3dd1b04e2b13e32a7951eeaa5d68980137e7
msieber88@gmail.com <msieber88@gmail.com@4feab3de-de25-87cd-0e07-2f1f9e466e77>
2011-05-25 07:10:43 +0000

    primefaces

---
+++ src/at/easydiet/teamc/controller/WebController.java
@@ -0,0 +1,19 @@
+/**
+ * This File is part of EasyDiet
+ * Created on: 25.05.2011
+ * Created by: Michael
+ * File: WebController.java
+ */
+package at.easydiet.teamc.controller;
+
+/**
+ * 
+ * @author Michael
+ */
+public class WebController {
+
+	// class variables
+	private static final org.apache.log4j.Logger LOGGER = org.apache.log4j.Logger
+			.getLogger(WebController.class);
+	// instance variables
+}
